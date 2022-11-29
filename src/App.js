a273f6d30f63f39f18a0a00c4cd52caf9496c6c4
Ali Ali <aliali_266@yahoo.com>
2022-11-28 22:08:45 -0800

    footer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
+// import React from 'react';
+// import Navbar from "./components/Navbar";
+// import './App.css';
+// import { Routes, Route } from "react-router-dom";
+// import Services from './components/pages/Services';
+// import Products from './components/pages/Products';
+// import SignUp from './components/pages/SignUp';
+// // import Home from './Components/pages/Home';
+// import Home from './Components/pages/Home';
+
+
+// function App() {
+//   return (
+//     <>
+
+//         <Navbar />
+//         <Routes>
+//           <Route path='/' element={<Home/>}/>
+//           <Route path='/services' element={<Services/>} />
+//           <Route path='/products' element={<Products/>} />
+//           <Route path='/sign-up' element={<SignUp/>} />
+//         </Routes>
+
+//     </>
+//   );
+// }
+
+// export default App;
+
 import React from 'react';
 import Navbar from "./components/Navbar";
 import './App.css';
-import Home from "./components/pages/Home";
 import { Routes, Route } from "react-router-dom";
+import Home from './components/pages/Home';
 import Services from './components/pages/Services';
 import Products from './components/pages/Products';
 import SignUp from './components/pages/SignUp';
@@ -13,7 +42,7 @@ function App() {
 
         <Navbar />
         <Routes>
-          <Route path='/' exact element={<Home/>}/>
+          <Route path='/' exact element={<Home/>} />
           <Route path='/services' element={<Services/>} />
           <Route path='/products' element={<Products/>} />
           <Route path='/sign-up' element={<SignUp/>} />
